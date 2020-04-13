9a3de85fb65f54daa19ddb94adee42c511057872
Ingrid <intarga@protonmail.com>
2020-04-13 19:39:12 +0200

    added documentation for the trie dictionary and plugged a memory leak.

--- triedictionary.c
+++ triedictionary.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//struct to represent a node in a trie. A root node is used to represent the trie as a whole.
 struct trieNode {
     struct trieNode* children[26];
     char* definition;
 };
 
+//allocates memory for a trie node and nulls the field pointers. Returns a pointer to that memory.
 struct trieNode* getNode() {
     struct trieNode* node = malloc(sizeof(struct trieNode));
 
@@ -19,53 +21,74 @@ struct trieNode* getNode() {
     return node;
 }
 
+//looks up the definition to a word in a trie, and returns that definition.
 char* lookup(struct trieNode* root, char* word) {
     struct trieNode* currNode = root;
 
+    //walking the trie to reach the node that will contain the definition.
     for(int i = 0; i < strlen(word); i++) {
         if(!currNode->children[word[i] - 'a'])
+            //return early if the path is incomplete, as the definition can't exist.
             return "no definition found";
         currNode = currNode->children[word[i] - 'a'];
     }
 
+    //return the definition if it exists.
     if(currNode->definition)
         return currNode->definition;
     return "no definition found";
 }
 
+//inserts a definition for a word into a trie, if it does not already exist.
 void insert(struct trieNode* root, char* word, char* definition) {
     struct trieNode* currNode = root;
 
+    //walking the trie to reach the node that will contain the definition.
     for(int i = 0; i < strlen(word); i++) {
         if(!currNode->children[word[i] - 'a'])
+            //create new nodes if the path is incomplete.
             currNode->children[word[i] - 'a'] = getNode();
         currNode = currNode->children[word[i] - 'a'];
     }
 
+    //if the definition doesn't already exist, allocate memory for it, and point to it.
     if(!currNode->definition)
         currNode->definition = malloc(strlen(definition) + 1);
         strcpy(currNode->definition, definition);
 }
 
+//deletes the definition of a word from a trie, and clean up the nodes this obsolesces.
 void delete(struct trieNode* root, char* word) {
     int wordLen = strlen(word);
+    //create an array to hold pointers to the trie nodes that make up the path to where the definition is found.
     struct trieNode** nodes = malloc((wordLen+1)*sizeof(struct trieNode*));
+    //the first node in this array is obviously the root.
     nodes[0] = root;
 
+    //walking the trie to reach the node that will contain the definition, while placing nodes into the path array.
     for(int i = 0; i < wordLen; i++) {
         nodes[i+1] = nodes[i]->children[word[i] - 'a'];
         if(!nodes[i+1])
-            break;
+            //if the path is incomplete return early, as the definiton can't exist.
+            return;
     }
 
+    //for the last node, check if it has any children
     for(int i = 0; i < 26; i++) {
         if(nodes[wordLen]->children[i]) {
+            //if it has, just delete the definition and null the pointer to it.
+            free(nodes[wordLen]->definition);
             nodes[wordLen]->definition = NULL;
+            //then return early as the path must remain.
             return;
         }
     }
 
+    //delete the definition and null the pointer to it.
+    free(nodes[wordLen]->definition);
     free(nodes[wordLen]);
+
+    //walk the path in reverse, cleaning up the nodes until we hit one that has a definition, or is the root.
     for(int i = wordLen - 1; i >= 0; i--) {
         if(nodes[i]->definition){
             nodes[i]->children[word[i] - 'a'] = NULL;
