7ea375206e1054f02a4231788f78edfe089b496f
Ingrid <intarga@protonmail.com>
2020-04-13 19:47:28 +0200

    added documentation for the linear string sort

--- linearstringsort.c
+++ linearstringsort.c
@@ -2,16 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
+//takes a string, and returns a lexically sorted copy in O(n) time.
 char* sortString(char inString[]) {
     int strLength = strlen(inString);
+    //setting up an int array to store the number of appearances of each character in the string.
     int charCounts[256] = {0};
 
+    //walk the string populating the above array.
     for(int i = 0; i < strLength; i++) {
         charCounts[inString[i]]++;
     }
 
+    //allocate memory for the output string.
     char* outString = malloc(strLength + 1);
 
+    //walk the appearance count array, to put characters in the output string in lexical order.
     int k = 0;
     for(int i = 0; i < 256; i++) {
         for(int j = 0; j < charCounts[i]; j++) {
@@ -19,13 +24,16 @@ char* sortString(char inString[]) {
             k++;
         }
     }
+    //terminate the string.
     outString[strLength] = '\0';
 
     return outString;
 }
 
 int main(int argc, char* argv[]) {
+    //only run with 1 command line argument (the string to be sorted).
     if(argc == 2) {
+        //sort the string and print the sorted string to stdout.
         printf("%s\n", sortString(argv[1]));
     }
 
