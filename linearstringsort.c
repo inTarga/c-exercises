84cb2ac9272af612c885c884b979be6e9929dcec
Ingrid <intarga@protonmail.com>
2020-04-14 18:31:30 +0200

    formatting changes

--- linearstringsort.c
+++ linearstringsort.c
@@ -3,37 +3,32 @@
 #include <stdlib.h>
 
 //takes a string, and returns a lexically sorted copy in O(n) time.
-char* sortString(char inString[]) {
+char *sortString(char inString[]) {
     size_t strLength = strlen(inString);
     //setting up an int array to store the number of appearances of each character in the string.
     int charCounts[256] = {0};
 
     //walk the string populating the above array.
-    for(int i = 0; i < strLength; i++) {
+    for (int i = 0; i < strLength; i++)
         charCounts[inString[i]]++;
-    }
 
     //allocate memory for the output string.
-    char* outString = malloc(strLength + 1);
+    char *outString = malloc(strLength + 1);
 
     //walk the appearance count array, to put characters in the output string in lexical order.
     int k = 0;
-    for(int i = 0; i < 256; i++) {
-        for(int j = 0; j < charCounts[i]; j++) {
+    for (int i = 0; i < 256; i++)
+        for (int j = 0; j < charCounts[i]; j++, k++)
             outString[k] = i;
-            k++;
-        }
-    }
 
     return outString;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     //only run with 1 command line argument (the string to be sorted).
-    if(argc == 2) {
+    if (argc == 2)
         //sort the string and print the sorted string to stdout.
         printf("%s\n", sortString(argv[1]));
-    }
 
     return 0;
 }
--- triedictionary.c
+++ triedictionary.c
@@ -5,71 +5,71 @@
 
 //struct to represent a node in a trie. A root node is used to represent the trie as a whole.
 struct trieNode {
-    struct trieNode* children[26];
-    char* definition;
+    struct trieNode *children[26];
+    char *definition;
 };
 
 //allocates memory for a trie node and nulls the field pointers. Returns a pointer to that memory.
-struct trieNode* getNode() {
-    struct trieNode* node = malloc(sizeof(struct trieNode));
+struct trieNode *getNode() {
+    struct trieNode *node = malloc(sizeof(struct trieNode));
 
     node->definition = NULL;
 
-    for(int i = 0; i < 26; i++)
+    for (int i = 0; i < 26; i++)
         node->children[i] = NULL;
 
     return node;
 }
 
 //looks up the definition to a word in a trie, and returns that definition.
-char* lookup(struct trieNode* root, char* word) {
-    struct trieNode* currNode = root;
+char *lookup(struct trieNode *root, char *word) {
+    struct trieNode *currNode = root;
 
     //walking the trie to reach the node that will contain the definition.
-    for(int i = 0; i < strlen(word); i++) {
-        if(!currNode->children[word[i] - 'a'])
+    for (int i = 0; i < strlen(word); i++) {
+        if (!currNode->children[word[i] - 'a'])
             //return early if the path is incomplete, as the definition can't exist.
             return "no definition found";
         currNode = currNode->children[word[i] - 'a'];
     }
 
     //return the definition if it exists.
-    if(currNode->definition)
+    if (currNode->definition)
         return currNode->definition;
     return "no definition found";
 }
 
 //inserts a definition for a word into a trie, if it does not already exist.
-void insert(struct trieNode* root, char* word, char* definition) {
-    struct trieNode* currNode = root;
+void insert(struct trieNode *root, char *word, char *definition) {
+    struct trieNode *currNode = root;
 
     //walking the trie to reach the node that will contain the definition.
-    for(int i = 0; i < strlen(word); i++) {
-        if(!currNode->children[word[i] - 'a'])
+    for (int i = 0; i < strlen(word); i++) {
+        if (!currNode->children[word[i] - 'a'])
             //create new nodes if the path is incomplete.
             currNode->children[word[i] - 'a'] = getNode();
         currNode = currNode->children[word[i] - 'a'];
     }
 
     //if the definition doesn't already exist, allocate memory for it, and point to it.
-    if(!currNode->definition) {
+    if (!currNode->definition) {
         currNode->definition = malloc(strlen(definition) + 1);
         strcpy(currNode->definition, definition);
     }
 }
 
 //deletes the definition of a word from a trie, and clean up the nodes this obsolesces.
-void delete(struct trieNode* root, char* word) {
+void delete(struct trieNode* root, char *word) {
     size_t wordLen = strlen(word);
     //create an array to hold pointers to the trie nodes that make up the path to where the definition is found.
-    struct trieNode** nodes = malloc((wordLen+1)*sizeof(struct trieNode*));
+    struct trieNode **nodes = malloc((wordLen+1)*sizeof(struct trieNode*));
     //the first node in this array is obviously the root.
     nodes[0] = root;
 
     //walking the trie to reach the node that will contain the definition, while placing nodes into the path array.
-    for(int i = 0; i < wordLen; i++) {
+    for (int i = 0; i < wordLen; i++) {
         nodes[i+1] = nodes[i]->children[word[i] - 'a'];
-        if(!nodes[i+1]) {
+        if (!nodes[i+1]) {
             //if the path is incomplete return early, as the definiton can't exist.
             free(nodes);
             return;
@@ -77,8 +77,8 @@ void delete(struct trieNode* root, char* word) {
     }
 
     //for the last node, check if it has any children
-    for(int i = 0; i < 26; i++) {
-        if(nodes[wordLen]->children[i]) {
+    for (int i = 0; i < 26; i++) {
+        if (nodes[wordLen]->children[i]) {
             //if it has, just delete the definition and null the pointer to it.
             free(nodes[wordLen]->definition);
             nodes[wordLen]->definition = NULL;
@@ -93,34 +93,33 @@ void delete(struct trieNode* root, char* word) {
     free(nodes[wordLen]);
 
     //walk the path in reverse, cleaning up the nodes until we hit one that has a definition, or is the root.
-    for(int i = wordLen - 1; i >= 0; i--) {
-        if(nodes[i]->definition){
+    for (int i = wordLen - 1; i >= 0; i--) {
+        if (nodes[i]->definition){
             nodes[i]->children[word[i] - 'a'] = NULL;
             break;
         }
-        if(i == 0) {
+        if (i == 0)
             nodes[i]->children[word[i] - 'a'] = NULL;
-        } else {
+        else
             free(nodes[i]);
-        }
     }
 
     free(nodes);
 }
 
 int main() {
-    struct trieNode* root = getNode();
+    struct trieNode *root = getNode();
     char command[7];
     char word[20];
     char definition[20];
 
     printf("Welcome to the trie dictionary. ");
 
-    while(true) {
+    while (true) {
         printf("Please input one of the following commands:\n  insert, lookup, delete, quit.\n>");
         scanf("%7s", command);
 
-        if(!strcmp(command, "insert")) {
+        if (!strcmp(command, "insert")) {
             printf("\nInput a word to insert.\n>");
             scanf("%20s", word);
             //check validity of word?
